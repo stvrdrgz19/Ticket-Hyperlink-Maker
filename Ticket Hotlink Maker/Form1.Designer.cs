edcdae8a54e474d23379a21cece78fc00a118bb4
stvrdrgz19 <stvrdrgz@gmail.com>
2020-06-05 14:12:33 -0400

    First commit

---
+++ Ticket Hotlink Maker/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Ticket_Hotlink_Maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbText);
+            this.groupBox1.Controls.Add(this.btnProcess);
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(568, 206);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Text To Process";
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(7, 20);
+            this.tbText.Multiline = true;
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(553, 148);
+            this.tbText.TabIndex = 0;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnProcess.Location = new System.Drawing.Point(7, 174);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(553, 23);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbResult);
+            this.groupBox2.Controls.Add(this.btnCopy);
+            this.groupBox2.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox2.Location = new System.Drawing.Point(4, 216);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(568, 206);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(7, 19);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(553, 148);
+            this.tbResult.TabIndex = 2;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCopy.Location = new System.Drawing.Point(7, 173);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(553, 23);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 425);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ticket Hyperlink Maker (THM)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ Ticket Hotlink Maker/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Ticket_Hotlink_Maker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (Control.ModifierKeys == Keys.Shift)
+            {
+                MessageBox.Show(Environment.CurrentDirectory);
+                return;
+                //if (!Directory.Exists(Environment.CurrentDirectory + "\\Logs"))
+                //{
+                //    MessageBox.Show("It doesn't exists!");
+                //    return;
+                //}
+            }
+            string var1 = @"=HYPERLINK(""https://salespad.atlassian.net/browse/";
+            string var2 = @""")";
+            List<string> rowList = new List<string>();
+            List<string> finalList = new List<string>();
+
+            // CHECK IF START TEXTBOX IS EMPTY
+            if (String.IsNullOrWhiteSpace(tbText.Text))
+            {
+                string errorMessage = "Please enter text to process!";
+                string errorCaption = "ERROR";
+                MessageBoxButtons errorButton = MessageBoxButtons.OK;
+                MessageBoxIcon errorIcon = MessageBoxIcon.Error;
+                DialogResult errorResult;
+
+                errorResult = MessageBox.Show(errorMessage, errorCaption, errorButton, errorIcon);
+                return;
+            }
+            // ACTIONS IF START TEXTBOX ISN'T EMPTY
+            var result = tbText.Text.Split(new[] { '\n' });
+            foreach (string x in result)
+            {
+                string sep = "\t";
+                string[] col = x.Split(sep.ToCharArray());
+                string y = var1 + col[0] + "\",\"" + col[1].Replace("\"", "\"\"") + var2 + "\t\t" + "Incomplete" + "\t\t\t\t" + col[2].Replace(";", " / ");
+                rowList.Add(y);
+            }
+
+            // POPULATE RESULT TEXTBOX WITH FORMATTED TEXT
+            string output = string.Join("\n", rowList.ToArray());
+            string outputx = output.Replace("\n\n", "\n");
+            tbResult.Text = outputx;
+            return;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            // CHECK IF RESULTS IS EMPTY
+            if (String.IsNullOrWhiteSpace(tbResult.Text))
+            {
+                string errorMessage = "There are no results to copy!";
+                string errorCaption = "ERROR";
+                MessageBoxButtons errorButton = MessageBoxButtons.OK;
+                MessageBoxIcon errorIcon = MessageBoxIcon.Error;
+                DialogResult errorResult;
+
+                errorResult = MessageBox.Show(errorMessage, errorCaption, errorButton, errorIcon);
+                return;
+            }
+            // COPY RESULTS CONTENTS TO CLIPBOARD
+            System.Windows.Forms.Clipboard.SetText(tbResult.Text);
+
+            // INFORM THE USER THAT THE RESULTS WERE COPIED
+            string successMessage = "Results were copied to the clipboard!";
+            string successCaption = "SUCCESS";
+            MessageBoxButtons successButton = MessageBoxButtons.OK;
+            MessageBoxIcon successIcon = MessageBoxIcon.Exclamation;
+            DialogResult successResult;
+
+            successResult = MessageBox.Show(successMessage, successCaption, successButton, successIcon);
+            return;
+        }
+    }
+}
